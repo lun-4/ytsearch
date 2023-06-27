c568cb3e3977995914ae1497beb5e7464b250e75
Luna <git@l4.pm>
2023-06-26 22:30:25 -0300

    add quest 2 support

--- lib/yt_search_web/controllers/slot_controller.ex
+++ lib/yt_search_web/controllers/slot_controller.ex
@@ -11,9 +11,35 @@ defmodule YtSearchWeb.SlotController do
         |> put_status(404)
 
       slot ->
-        # TODO verify user agent
-        conn
-        |> redirect(external: "https://youtube.com/watch?v=#{slot.youtube_id}")
+        agent =
+          case get_req_header(conn, "user-agent") do
+            [] -> ""
+            v -> Enum.at(v, 0)
+          end
+
+        youtube_url = "https://youtube.com/watch?v=#{slot.youtube_id}"
+
+        if String.contains?(agent, "stagefright") or String.contains?(agent, "AVProMobileVideo") do
+          # get mp4 from ytdlp
+          {output, exit_status} =
+            System.cmd("yt-dlp", [
+              "--no-check-certificate",
+              "--no-cache-dir",
+              "--rm-cache-dir",
+              "-f",
+              "mp4[height<=?1080][height>=?64][width>=?64]/best[height<=?1080][height>=?64][width>=?64]",
+              "--get-url",
+              youtube_url
+            ])
+
+          mp4_url = String.trim(output)
+
+          conn
+          |> redirect(external: mp4_url)
+        else
+          conn
+          |> redirect(external: youtube_url)
+        end
     end
   end
 end
