7d93e417d1adffa75de6433ba397c2a921aa4449
Luna <git@l4.pm>
2023-07-01 01:57:02 -0300

    explicitly make video shorts' uploader optional
    youtube is sus etc

--- lib/yt_search_web/playlist.ex
+++ lib/yt_search_web/playlist.ex
@@ -1,5 +1,6 @@
 defmodule YtSearchWeb.Playlist do
   alias YtSearch.Youtube
+  require Logger
 
   def from_ytdlp_data(youtube_json_results) do
     youtube_json_results
@@ -22,17 +23,32 @@ defmodule YtSearchWeb.Playlist do
     |> Enum.map(fn {entity_type, ytdlp_data} ->
       thumbnail_metadata = Youtube.Thumbnail.fetch_in_background(ytdlp_data)
 
+      Logger.debug("parsing #{inspect(ytdlp_data)}")
+
       case entity_type do
         t when t == :video or t == :short ->
           slot = YtSearch.Slot.from(ytdlp_data["id"])
 
+          # when using /channel/videos, channel_id is null, so
+          # fallback to playlist_uploader_id in these cases
+          channel_id = ytdlp_data["channel_id"] || ytdlp_data["playlist_uploader_id"]
+
           channel_slot =
-            if ytdlp_data["channel_id"] != nil do
-              YtSearch.ChannelSlot.from(ytdlp_data["channel_id"])
-            else
-              # when using /channel/videos, channel_id is null, so
-              # fallback to playlist_uploader_id in these cases
-              YtSearch.ChannelSlot.from(ytdlp_data["playlist_uploader_id"])
+            case entity_type do
+              :video ->
+                # full videos should provide channel metadata
+                # from/1 will crash if its nil
+                YtSearch.ChannelSlot.from(channel_id)
+
+              :short ->
+                # shorts dont give proper metadata about themselves at all
+                # fuck shorts
+                # make it optional
+                if channel_id == nil do
+                  nil
+                else
+                  YtSearch.ChannelSlot.from(channel_id)
+                end
             end
 
           channel_name = ytdlp_data["channel"] || ytdlp_data["playlist_uploader_name"]
