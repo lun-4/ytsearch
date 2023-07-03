895be1520e010041582cb528cd860c4467089d39
Luna <git@l4.pm>
2023-07-03 20:40:24 -0300

    fix slot test not being reproducible after a day

--- test/yt_search_web/controllers/slot_test.exs
+++ test/yt_search_web/controllers/slot_test.exs
@@ -10,8 +10,11 @@ defmodule YtSearchWeb.SlotTest do
     %{slot: slot}
   end
 
+  @custom_expire (System.os_time(:second) + 3_600) |> to_string
+
   @run1 File.read!("test/support/files/stdout_ytdlp_geturl_dumpjson.txt")
-  @run1_url_result "https://rr2---sn-oxunxg8pjvn-gxjl.googlevideo.com/videoplayback?expire=1688360198&ei=pgCiZNDVBe2pobIPiq6pqA8&ip=2804%3A14d%3A5492%3A8fe8%3A%3A1003&id=o-ANdnakoQKlSGp-uvkmjmZa5oqJUed6hsUz7eS-e_g320&itag=22&source=youtube&requiressl=yes&mh=A0&mm=31%2C29&mn=sn-oxunxg8pjvn-gxjl%2Csn-gpv7kn7r&ms=au%2Crdu&mv=m&mvi=2&pl=52&initcwndbps=478750&spc=Ul2SqzJcCc2KHSrHFBWlfSlG7kQZPSM&vprv=1&svpuc=1&mime=video%2Fmp4&cnr=14&ratebypass=yes&dur=339.057&lmt=1673716310924231&mt=1688338410&fvip=2&fexp=24007246%2C24363393%2C51000014&c=ANDROID&txp=5432434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Ccnr%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIgYoYhyrDOKJLsyS1SzRqXRRM6Z4rkRE0RU6kWNwFqY38CIQCPdGAN2OCTyzj_AZdc7s5PyS9JjQqZFnSopbKbS6itpw%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRQIhALdIfWCgI3dyh6OyJaUp_Qx7P4-sAW8iVTHaC7dobJw8AiBGIcUd6UtnawUTDsDrjyn9yB8DPoJycpeuF-ZRDqB7Yw%3D%3D"
+        |> String.replace("1688360198", @custom_expire)
+  @run1_url_result "https://rr2---sn-oxunxg8pjvn-gxjl.googlevideo.com/videoplayback?expire=#{@custom_expire}&ei=pgCiZNDVBe2pobIPiq6pqA8&ip=2804%3A14d%3A5492%3A8fe8%3A%3A1003&id=o-ANdnakoQKlSGp-uvkmjmZa5oqJUed6hsUz7eS-e_g320&itag=22&source=youtube&requiressl=yes&mh=A0&mm=31%2C29&mn=sn-oxunxg8pjvn-gxjl%2Csn-gpv7kn7r&ms=au%2Crdu&mv=m&mvi=2&pl=52&initcwndbps=478750&spc=Ul2SqzJcCc2KHSrHFBWlfSlG7kQZPSM&vprv=1&svpuc=1&mime=video%2Fmp4&cnr=14&ratebypass=yes&dur=339.057&lmt=1673716310924231&mt=1688338410&fvip=2&fexp=24007246%2C24363393%2C51000014&c=ANDROID&txp=5432434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Ccnr%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIgYoYhyrDOKJLsyS1SzRqXRRM6Z4rkRE0RU6kWNwFqY38CIQCPdGAN2OCTyzj_AZdc7s5PyS9JjQqZFnSopbKbS6itpw%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRQIhALdIfWCgI3dyh6OyJaUp_Qx7P4-sAW8iVTHaC7dobJw8AiBGIcUd6UtnawUTDsDrjyn9yB8DPoJycpeuF-ZRDqB7Yw%3D%3D"
   @run2 @run1 |> String.replace(@run1_url_result, "https://mp5.com")
 
   test "it gets the mp4 url on quest useragents, supporting ttl", %{conn: conn, slot: slot} do
