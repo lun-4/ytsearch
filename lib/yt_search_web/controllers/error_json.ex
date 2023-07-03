fe50eb15509c7146de35d53272dbba0eb629b88f
Luna <git@l4.pm>
2023-07-02 23:18:51 -0300

    add stack trace to 500.json

--- lib/yt_search_web/controllers/error_json.ex
+++ lib/yt_search_web/controllers/error_json.ex
@@ -9,6 +9,25 @@ defmodule YtSearchWeb.ErrorJSON do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.json" becomes
   # "Not Found".
+  def render("500.json", assigns) do
+    %{
+      errors: %{
+        detail: Phoenix.Controller.status_message_from_template("500.json"),
+        technical_detail: %{
+          reason: assigns.reason |> inspect(),
+          stack:
+            assigns.stack
+            |> Enum.map(fn {module, function, arity, location} ->
+              %{
+                where: "#{inspect(module)}.#{inspect(function)}/#{arity}",
+                location: inspect(location)
+              }
+            end)
+        }
+      }
+    }
+  end
+
   def render(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
