79e4370ae937846c73fed1d425f6c71a9c52b00c
Luna <git@l4.pm>
2023-08-10 20:57:17 -0300

    fix assert image not working on WEBP

--- test/support/assert_util.ex
+++ test/support/assert_util.ex
@@ -4,9 +4,16 @@ defmodule YtSearch.AssertUtil do
   def image(temporary_path) do
     {output, 0} = System.cmd("identify", [temporary_path])
 
+    split_word =
+      cond do
+        String.contains?(output, "PNG") -> "PNG"
+        String.contains?(output, "WEBP") -> "WEBP"
+        true -> raise "invalid output: #{output}"
+      end
+
     [{width, ""}, {height, ""}] =
       output
-      |> String.split("PNG")
+      |> String.split(split_word)
       |> Enum.at(1)
       |> String.trim(" ")
       |> String.split(" ")
