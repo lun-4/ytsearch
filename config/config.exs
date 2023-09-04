1140889dbbdd20b2060ff222577e08711a74e846
Luna <git@l4.pm>
2023-09-04 20:33:49 -0300

    exclude StaleEntryError from PhoenixEcto translations

--- config/config.exs
+++ config/config.exs
@@ -75,6 +75,9 @@ config :tesla, adapter: Tesla.Adapter.Hackney
 
 config :yt_search, YtSearch.ThumbnailAtlas, montage_command: "montage"
 
+config :phoenix_ecto,
+  exclude_ecto_exceptions_from_plug: [Ecto.StaleEntryError]
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
