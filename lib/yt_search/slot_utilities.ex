86067f1eb036624540ae6e3aac033e0bb40fa864
Luna <git@l4.pm>
2023-09-13 22:46:36 -0300

    Revert "delete top-N oldest slots instead of just one of them"
    This reverts commit 6c50f4e98757dd2d4370c05ceee581c0833f03cf.

--- lib/yt_search/slot_utilities.ex
+++ lib/yt_search/slot_utilities.ex
@@ -1,6 +1,5 @@
 defmodule YtSearch.SlotUtilities do
   import Ecto.Query
-  require Logger
   alias YtSearch.Repo
   alias YtSearch.TTL
 
@@ -63,7 +62,7 @@ defmodule YtSearch.SlotUtilities do
         order_by: [
           asc: s.inserted_at_v2
         ],
-        limit: 30
+        limit: 50
 
     Repo.all(query)
     |> then(fn
@@ -71,46 +70,12 @@ defmodule YtSearch.SlotUtilities do
         raise "we should have already generated an entity id here"
 
       rows ->
-        # the problem statement here is that a request may attempt to use
-        # the slot id another request already deleted. this code path only
-        # happens in periods of high slot contention (AKA peak time for YTS).
-
-        # when the "best-case" id generation fails (random id), we fall to here
-        # which *has* to find an id out. the old solution used the following algorithm:
-        #  1. select 1 from the top-N oldest slots
-        #  2. delete it, then use its slot id
-
-        # this causes a race condition where one request may use the slot that's
-        # already deleted by another request. the probability is 1/N, and we
-        # attempted to raise N (from 1 to 30 to 50), to various degrees of success
-
-        # to decrease the probability further while keeping the "worst-case" performance
-        # within limits, the algorithm is as follows:
-        #  1. select top-N oldest slots
-        #  2. delete all of them
-        #  3. choose random id from there
-
-        # over time, the id contention window for requests will be lower, because:
-        #  1. deleting top-N frees those ids, so they can be used again on the "best-case" id generator
-        #  2. if it's under contention, it's highly unlikely two requests will get the
-        #     same set of top-N-oldest slots (one of them will have deleted all of them),
-        #     so a third request that is operating under contention will have a different
-        #     view of top-N-oldest
-        chosen_entity =
+        entity =
           rows
-          |> Enum.map(fn entity ->
-            try do
-              Repo.delete(entity)
-            rescue
-              Ecto.StaleEntryError ->
-                Logger.error("got StaleEntryError while deleting #{inspect(entity)}, ignoring!")
-            end
-
-            entity
-          end)
           |> Enum.random()
 
-        {:ok, chosen_entity.id}
+        Repo.delete(entity)
+        {:ok, entity.id}
     end)
   end
 
@@ -121,7 +86,7 @@ defmodule YtSearch.SlotUtilities do
         order_by: [
           asc: fragment("unixepoch(?)", s.inserted_at)
         ],
-        limit: 30
+        limit: 50
 
     Repo.all(query)
     |> then(fn
@@ -129,21 +94,12 @@ defmodule YtSearch.SlotUtilities do
         raise "we should have already generated an entity id here"
 
       rows ->
-        chosen_entity =
+        entity =
           rows
-          |> Enum.map(fn entity ->
-            try do
-              Repo.delete(entity)
-            rescue
-              Ecto.StaleEntryError ->
-                Logger.error("got StaleEntryError while deleting #{inspect(entity)}, ignoring!")
-            end
-
-            entity
-          end)
           |> Enum.random()
 
-        {:ok, chosen_entity.id}
+        Repo.delete(entity)
+        {:ok, entity.id}
     end)
   end
 end
