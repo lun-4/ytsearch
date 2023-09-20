e7ed869cae6fc4b91dbb90d3e0a1efa90165cb0f
Luna <git@l4.pm>
2023-09-20 00:43:28 -0300

    run trending tab test 10 times

--- test/yt_search_web/controllers/trending_tab_test.exs
+++ test/yt_search_web/controllers/trending_tab_test.exs
@@ -10,74 +10,77 @@ defmodule YtSearchWeb.TrendingTabTest do
     YtSearch.Test.Data.default_global_mock()
   end
 
-  test "trending tab works", %{conn: conn} do
-    Tesla.Mock.mock(fn
-      %{method: :get, url: "example.org/trending", query: [region: "US"]} ->
-        Tesla.Mock.json(
-          @test_output
-          |> Jason.decode!()
+  1..10
+  |> Enum.each(fn num ->
+    test "trending tab works #{num}", %{conn: conn} do
+      Tesla.Mock.mock(fn
+        %{method: :get, url: "example.org/trending", query: [region: "US"]} ->
+          Tesla.Mock.json(
+            @test_output
+            |> Jason.decode!()
+          )
+      end)
+
+      conn =
+        conn
+        |> get(~p"/api/v3/hello")
+
+      resp_json = json_response(conn, 200)
+      results = resp_json["trending_tab"]["search_results"]
+      assert length(results) == 20
+      assert results |> Enum.at(0) |> Map.get("youtube_id") == "HYzyRHAHJl8"
+      assert results |> Enum.at(3) |> Map.get("youtube_id") == "AsvGScyj4gw"
+
+      {slot_id, ""} = results |> Enum.at(0) |> Map.get("slot_id") |> Integer.parse()
+      {channel_slot_id, ""} = results |> Enum.at(0) |> Map.get("channel_slot") |> Integer.parse()
+
+      slot = Slot.fetch_by_id(slot_id)
+
+      slot_before =
+        slot
+        |> Ecto.Changeset.change(
+          expires_at:
+            NaiveDateTime.utc_now() |> NaiveDateTime.add(-30) |> NaiveDateTime.truncate(:second)
         )
-    end)
+        |> YtSearch.Repo.update!()
 
-    conn =
-      conn
-      |> get(~p"/api/v3/hello")
+      channel_slot_before =
+        ChannelSlot.fetch(channel_slot_id)
+        |> Ecto.Changeset.change(
+          inserted_at: slot.inserted_at |> NaiveDateTime.add(-(ChannelSlot.ttl() + 1), :second)
+        )
+        |> YtSearch.Repo.update!()
 
-    resp_json = json_response(conn, 200)
-    results = resp_json["trending_tab"]["search_results"]
-    assert length(results) == 20
-    assert results |> Enum.at(0) |> Map.get("youtube_id") == "HYzyRHAHJl8"
-    assert results |> Enum.at(3) |> Map.get("youtube_id") == "AsvGScyj4gw"
+      search_slot_id = resp_json["trending_tab"]["slot_id"]
 
-    {slot_id, ""} = results |> Enum.at(0) |> Map.get("slot_id") |> Integer.parse()
-    {channel_slot_id, ""} = results |> Enum.at(0) |> Map.get("channel_slot") |> Integer.parse()
+      search_slot_before =
+        SearchSlot.fetch_by_id(search_slot_id)
+        |> Ecto.Changeset.change(
+          inserted_at: slot.inserted_at |> NaiveDateTime.add(-(SearchSlot.ttl() + 1), :second)
+        )
+        |> YtSearch.Repo.update!()
 
-    slot = Slot.fetch_by_id(slot_id)
+      YtSearchWeb.HelloController.Refresher.tick()
 
-    slot_before =
-      slot
-      |> Ecto.Changeset.change(
-        expires_at:
-          NaiveDateTime.utc_now() |> NaiveDateTime.add(-30) |> NaiveDateTime.truncate(:second)
-      )
-      |> YtSearch.Repo.update!()
+      search_slot_after = SearchSlot.fetch_by_id(search_slot_id)
+      assert search_slot_after != nil
 
-    channel_slot_before =
-      ChannelSlot.fetch(channel_slot_id)
-      |> Ecto.Changeset.change(
-        inserted_at: slot.inserted_at |> NaiveDateTime.add(-(ChannelSlot.ttl() + 1), :second)
-      )
-      |> YtSearch.Repo.update!()
+      slot_after = Slot.fetch_by_id(slot_id)
+      assert slot_after.expires_at > slot_before.expires_at
+      assert search_slot_after.inserted_at > search_slot_before.inserted_at
 
-    search_slot_id = resp_json["trending_tab"]["slot_id"]
+      channel_slot_after = ChannelSlot.fetch(channel_slot_id)
+      assert channel_slot_after.inserted_at > channel_slot_before.inserted_at
 
-    search_slot_before =
-      SearchSlot.fetch_by_id(search_slot_id)
-      |> Ecto.Changeset.change(
-        inserted_at: slot.inserted_at |> NaiveDateTime.add(-(SearchSlot.ttl() + 1), :second)
-      )
-      |> YtSearch.Repo.update!()
+      # re-request it
+      conn =
+        conn
+        |> get(~p"/api/v3/hello")
 
-    YtSearchWeb.HelloController.Refresher.tick()
-
-    search_slot_after = SearchSlot.fetch_by_id(search_slot_id)
-    assert search_slot_after != nil
-
-    slot_after = Slot.fetch_by_id(slot_id)
-    assert slot_after.expires_at > slot_before.expires_at
-    assert search_slot_after.inserted_at > search_slot_before.inserted_at
-
-    channel_slot_after = ChannelSlot.fetch(channel_slot_id)
-    assert channel_slot_after.inserted_at > channel_slot_before.inserted_at
-
-    # re-request it
-    conn =
-      conn
-      |> get(~p"/api/v3/hello")
-
-    resp_json = json_response(conn, 200)
-    results2 = resp_json["trending_tab"]["search_results"]
-    assert results2 |> Enum.at(0) == results |> Enum.at(0)
-    assert results2 |> Enum.at(3) == results |> Enum.at(3)
-  end
+      resp_json = json_response(conn, 200)
+      results2 = resp_json["trending_tab"]["search_results"]
+      assert results2 |> Enum.at(0) == results |> Enum.at(0)
+      assert results2 |> Enum.at(3) == results |> Enum.at(3)
+    end
+  end)
 end
