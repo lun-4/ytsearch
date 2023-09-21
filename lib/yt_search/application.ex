d9d59880acac86941b73dbc1de24947b379a5cda
Luna <git@l4.pm>
2023-09-21 20:32:46 -0300

    remove now-unused trending tab refresher cron job

--- lib/yt_search/application.ex
+++ lib/yt_search/application.ex
@@ -62,7 +62,6 @@ defmodule YtSearch.Application do
         # Tinycron.new(YtSearch.Slot.Janitor, every: 10 * 60, jitter: (-3 * 60)..(3 * 60)),
         Tinycron.new(YtSearch.Subtitle.Cleaner, every: 8 * 60, jitter: -60..60),
         Tinycron.new(YtSearch.Mp4Link.Janitor, every: 10 * 60, jitter: (-2 * 60)..(5 * 60)),
-        Tinycron.new(YtSearchWeb.HelloController.Refresher, every: 3 * 60, jitter: -60..60),
         Tinycron.new(YtSearch.Thumbnail.Janitor, every: 10 * 60, jitter: (-2 * 60)..(4 * 60)),
         Tinycron.new(YtSearch.Repo.Analyzer, every: 3 * 60 * 60, jitter: (-20 * 60)..(20 * 60))
       ]
