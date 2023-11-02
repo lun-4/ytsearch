fb620c4b6ceef1bebd2e34901d381ab224ca2472
Luna <git@l4.pm>
2023-11-01 22:35:29 -0300

    add mogrify to thumbnail test command

--- lib/mix/thumbnail_test.ex
+++ lib/mix/thumbnail_test.ex
@@ -1,24 +1,41 @@
 defmodule Mix.Tasks.YtSearch.TestThumbnail do
   use Mix.Task
 
+  @mogrify false
+
   def run([path]) do
-    input_image = Image.open!(path)
+    if @mogrify do
+      temporary_path = Temp.path!()
+      File.write(temporary_path, File.read!(path))
 
-    input_image
-    |> Image.add_alpha(:transparent)
-    |> then(fn
-      {:ok, image} ->
-        image
+      Mogrify.open(temporary_path)
+      |> Mogrify.resize("256x144")
+      |> Mogrify.gravity("center")
+      |> Mogrify.custom("background", "none")
+      |> Mogrify.extent("256x144")
+      |> Mogrify.save(in_place: true)
+      |> dbg
 
-      {:error, "Image already has an alpha band"} ->
-        input_image
+      IO.inspect(temporary_path)
+    else
+      input_image = Image.open!(path)
 
-      {:error, err} ->
-        raise err
-    end)
-    |> Image.thumbnail!(256, height: 144)
-    |> Image.embed!(256, 144, background_transparency: 0, x: :center, y: :center)
-    |> Image.write!("output.png")
-    |> dbg
+      input_image
+      |> Image.add_alpha(:transparent)
+      |> then(fn
+        {:ok, image} ->
+          image
+
+        {:error, "Image already has an alpha band"} ->
+          input_image
+
+        {:error, err} ->
+          raise err
+      end)
+      |> Image.thumbnail!(256, height: 144)
+      |> Image.embed!(256, 144, background_transparency: 0, x: :center, y: :center)
+      |> Image.write!("output.webp")
+      |> dbg
+    end
   end
 end
