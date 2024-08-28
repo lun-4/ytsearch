105eea6fab96074fd332319de18f732156b77eec
Luna <git@l4.pm>
2024-08-27 23:39:28 -0300

    set redirect_to_googlevideo as false by default

--- config/config.exs
+++ config/config.exs
@@ -144,7 +144,7 @@ config :yt_search, YtSearch.Constants,
   minimum_time_between_refreshes: 60,
   enable_periodic_tasks: true,
   enable_periodic_janitors: true,
-  redirect_to_googlevideo?: true
+  redirect_to_googlevideo?: false
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
