3e691c9b9f4bfc2a2837d3cf325435215ab8fd8f
Luna <git@l4.pm>
2025-10-07 01:01:25 -0300

    sync conf w/ prod

--- config/config.exs
+++ config/config.exs
@@ -155,8 +155,8 @@ config :yt_search, YtSearch.Constants,
   enable_periodic_tasks: true,
   enable_periodic_janitors: true,
   redirect_to_googlevideo?: true,
-  extract_cta?: false,
-  thumbnails_in_search_page: 7
+  extract_cta?: true,
+  thumbnails_in_search_page: 20
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
