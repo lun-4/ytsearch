42dc07e517ddee860999950a0dd62922ace2fe3e
Luna <git@l4.pm>
2025-11-07 21:48:55 -0300

    attempt to free all thumbs

--- config/config.exs
+++ config/config.exs
@@ -156,7 +156,7 @@ config :yt_search, YtSearch.Constants,
   enable_periodic_janitors: true,
   redirect_to_googlevideo?: true,
   extract_cta?: true,
-  thumbnails_in_search_page: 20
+  thumbnails_in_search_page: 32
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
